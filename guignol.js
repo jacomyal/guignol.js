cf61fba04558fd0c5a5897a214e1037d89d0af03
Alexis Jacomy <alexis.jacomy@gmail.com>
2012-12-22 13:47:22 +0100

    first commit

---
+++ guignol.js
@@ -0,0 +1,236 @@
+(function(window, undefined) {
+  /**
+   * Naming policy:
+   * **************
+   *  - Upper case: Parameters recognized by Gepetto
+   *  - No underscore (prefix): var in local scope or parameter
+   *  - One underscore (prefix): Related to the instance
+   *  - Two underscores (prefix): Related to every instances
+   */
+  var __easings = {},
+      __renderers = {};
+      
+  /**
+   * Recognized parameters:
+   * **********************
+   *  - scenario: (?object)
+   *  - times: (?object)
+   *  - renderers: (?object)
+   *  - animations: (?object)
+   *  - start: (?number)
+   *  - end: (?number)
+   */
+  window.gepetto = function(options) {
+    var _o = options || {},
+        _times = _o.times || _o.t || {},
+        _renderers = _o.renderers || _o.r || {},
+        _animations = _o.animations || _o.a || {},
+        _scenario = _o.scenario || _o.s || {},
+        _start = _getTime(_o.start),
+        _end = _getTime(_o.end),
+        _originTime,
+        _time,
+        _isPlaying;
+
+    function _getTime(t) {
+      return typeof t === 'string' ?
+        _times[t] || null :
+        t;
+    }
+
+    function _getAnim(o) {
+      var res = {};
+
+      if (typeof o === 'string')
+        return _animations[o];
+      else if (typeof o === 'object') {
+        if (o.ANIMATION && _animations[o.ANIMATION]) {
+          var k,
+              res = {},
+              a = _animations[o.ANIMATION];
+          for (k in o)
+            res[k] = o[k];
+          for (k in a)
+            res[k] = a[k];
+
+          delete res.ANIMATION;
+          return res;
+        }
+      }
+
+      return o;
+    }
+
+    function _render(t) {
+      var i, s, r; // index, shape, renderer
+      t = t - _originTime + (_start || 0);
+
+      for (i in _scenario) {
+        s = _scenario[i];
+
+        // Check local, then global:
+        r = _renderers[s._renderer] || __renderers[s._renderer];
+
+        if (
+          r && (s.END === undefined || _getTime(s.END) > t) &&
+          (s.START === undefined || _getTime(s.START) < t)
+        ) {
+          r(__expand(a, t, _getTime, _getAnim));
+        }
+      }
+
+      if (_isPlaying && t < _end)
+        __requestAnimFrame(_render);
+      else if (t > _end)
+        _isPlaying = false
+    }
+
+    this.goTo = function(t) {
+      _time = t;
+      _render(t);
+    };
+    this.play = function(t) {
+      _originTime = new Date();
+      _isPlaying = true;
+      _render();
+    };
+    this.stop = function() {
+      _isPlaying = false;
+    };
+    this.time = function() {
+      return _time;
+    };
+  };
+  var G = window.guignol;
+
+  /**
+   * Find window.requestAnimationFrame fallback:
+   */
+  function __requestAnimFrame = (function() {
+    return  window.requestAnimationFrame || 
+            window.webkitRequestAnimationFrame || 
+            window.mozRequestAnimationFrame || 
+            window.oRequestAnimationFrame || 
+            window.msRequestAnimationFrame || 
+            function(callback) {
+              window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+
+  function __interpolate(from, to, progress) {
+    if (typeof from === 'object') {
+      var res = {};
+      for (var k in from)
+        res[k] = __interpolate(from[k], to[k], progress);
+      return res;
+    } else if (typeof from === 'number')
+      return from + (to - from) * progress;
+    else
+      return from;
+  }
+
+  /**
+   * Recognized parameters:
+   * **********************
+   *  - START: The start time of the animation (number|string)
+   *  - END: The end time of the animation (number|string)
+   *  - FROM: The initial value of the animation (number)
+   *  - TO: The final value of the animation (number)
+   *  - EASING: The easing of the animation (?function|string)
+   *  - ANIMATION: The parent animation (?string)
+   */
+  function __expand(v, t, getTime, getAnim) {
+    // If an array, find the good timeStep:
+    if (Object.prototype.toString.call(v) === '[object Array]') {
+      var i, oMin, oMax, s, e,
+          min = t, max = t;
+      for (i in v) {
+        var o = getAnim(v[i]),
+            s = getTime(o.START),
+            e = getTime(o.END);
+
+        // If a timestep matches, just interpolate
+        if (s <= t && e >= t)
+          return __interpolate(o.FROM, o.TO, t);
+
+        // If not, find the good persistent timestep
+        else {
+          if (s > min) {
+            min = s;
+            oMin = o;
+          } else if (e < max) {
+            max = e;
+            oMax = o;
+          }
+        }
+      }
+
+      if (oMax)
+        return oMax.TO;
+      else if (oMin)
+        return oMin.FROM;
+
+    // If an object, try to interpolate:
+    } else if (typeof v === 'object' || typeof v === 'string') {
+        var o = getAnim(v[i]),
+            s = getTime(o.START),
+            e = getTime(o.END);
+
+      // Let's first check if the object is a valid animation:
+      if (s !== undefined && e !== undefined) {
+        var progress = (t - s) / (e - s);
+        progress = Math.min(1, Math.max(0, progress));
+
+        if (o.EASING) {
+          if (__easings[o.EASING])
+            progress = __easings[o.EASING](progress);
+          else if (typeof o.EASING === 'function')
+            progress = o.EASING(progress);
+        }
+
+        return __interpolate(o.FROM, o.TO, progress);
+
+      // If not, let's expand sub-objects:
+      } else {
+        var res = {};
+
+        for (var k in o) {
+          res[k] = __expand(o[k], t);
+        }
+      }
+
+    // It not an object or an array, nothing to expand:
+    } else
+      return v;
+  }
+
+  G.requestAnimationFrame = __requestAnimFrame;
+  G.easings = function(a1, a2) {
+    if (arguments.length === 1) {
+      if (typeof a1 === 'object') {
+        for (var k in a1)
+          G.easings(k, a1[k]);
+      } else
+        return __easings[a1];
+    } else if (arguments.length === 2) {
+      if (typeof a2 !== 'function')
+        throw '[guignol] Easings must be functions.';
+      __easings[a1] = a2;
+    }
+    return this;
+  }
+  G.renderers = function(a1, a2) {
+    if (arguments.length === 1) {
+      if (typeof a1 === 'object') {
+        for (var k in a1)
+          G.renderers(k, a1[k]);
+      } else
+        return __renderers[a1];
+    } else if (arguments.length === 2) {
+      if (typeof a2 !== 'function')
+        throw '[guignol] Renderers must be functions.';
+      __renderers[a1] = a2;
+    }
+    return this;
+  }
+})(window);
---
+++ samples/easings.js
@@ -0,0 +1,135 @@
+(function(window) {
+  window.easings = window.easings || {};
+  var easings = window.easings;
+
+  easings.linearNone = function(k) {
+    return k;
+  };
+  easings.quadraticIn = function(k) {
+    return k * k;
+  };
+  easings.quadraticOut = function(k) {
+    return k * ( 2 - k );
+  };
+  easings.quadraticInOut = function(k) {
+    if ( ( k *= 2 ) < 1 ) return 0.5 * k * k;
+    return - 0.5 * ( --k * ( k - 2 ) - 1 );
+  };
+  easings.cubicIn = function(k) {
+    return k * k * k;
+  };
+  easings.cubicOut = function(k) {
+    return --k * k * k + 1;
+  };
+  easings.cubicInOut = function(k) {
+    if ( ( k *= 2 ) < 1 ) return 0.5 * k * k * k;
+    return 0.5 * ( ( k -= 2 ) * k * k + 2 );
+  };
+  easings.quarticIn = function(k) {
+    return k * k * k * k;
+  };
+  easings.quarticOut = function(k) {
+    return 1 - ( --k * k * k * k );
+  };
+  easings.quarticInOut = function(k) {
+    if ( ( k *= 2 ) < 1) return 0.5 * k * k * k * k;
+    return - 0.5 * ( ( k -= 2 ) * k * k * k - 2 );
+  };
+  easings.quinticIn = function(k) {
+    return k * k * k * k * k;
+  };
+  easings.quinticOut = function(k) {
+    return --k * k * k * k * k + 1;
+  };
+  easings.quinticInOut = function(k) {
+    if ( ( k *= 2 ) < 1 ) return 0.5 * k * k * k * k * k;
+    return 0.5 * ( ( k -= 2 ) * k * k * k * k + 2 );
+  };
+  easings.sinusoidalIn = function(k) {
+    return 1 - Math.cos( k * Math.PI / 2 );
+  };
+  easings.sinusoidalOut = function(k) {
+    return Math.sin( k * Math.PI / 2 );
+  };
+  easings.sinusoidalInOut = function(k) {
+    return 0.5 * ( 1 - Math.cos( Math.PI * k ) );
+  };
+  easings.exponentialIn = function(k) {
+    return k === 0 ? 0 : Math.pow( 1024, k - 1 );
+  };
+  easings.exponentialOut = function(k) {
+    return k === 1 ? 1 : 1 - Math.pow( 2, - 10 * k );
+  };
+  easings.exponentialInOut = function(k) {
+    if ( k === 0 ) return 0;
+    if ( k === 1 ) return 1;
+    if ( ( k *= 2 ) < 1 ) return 0.5 * Math.pow( 1024, k - 1 );
+    return 0.5 * ( - Math.pow( 2, - 10 * ( k - 1 ) ) + 2 );
+  };
+  easings.circularIn = function(k) {
+    return 1 - Math.sqrt( 1 - k * k );
+  };
+  easings.circularOut = function(k) {
+    return Math.sqrt( 1 - ( --k * k ) );
+  };
+  easings.circularInOut = function(k) {
+    if ( ( k *= 2 ) < 1) return - 0.5 * ( Math.sqrt( 1 - k * k) - 1);
+    return 0.5 * ( Math.sqrt( 1 - ( k -= 2) * k) + 1);
+  };
+  easings.elasticIn = function(k) {
+    var s, a = 0.1, p = 0.4;
+    if ( k === 0 ) return 0;
+    if ( k === 1 ) return 1;
+    if ( !a || a < 1 ) { a = 1; s = p / 4; }
+    else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
+    return - ( a * Math.pow( 2, 10 * ( k -= 1 ) ) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) );
+  };
+  easings.elasticOut = function(k) {
+    var s, a = 0.1, p = 0.4;
+    if ( k === 0 ) return 0;
+    if ( k === 1 ) return 1;
+    if ( !a || a < 1 ) { a = 1; s = p / 4; }
+    else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
+    return ( a * Math.pow( 2, - 10 * k) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) + 1 );
+  };
+  easings.elasticInOut = function(k) {
+    var s, a = 0.1, p = 0.4;
+    if ( k === 0 ) return 0;
+    if ( k === 1 ) return 1;
+    if ( !a || a < 1 ) { a = 1; s = p / 4; }
+    else s = p * Math.asin( 1 / a ) / ( 2 * Math.PI );
+    if ( ( k *= 2 ) < 1 ) return - 0.5 * ( a * Math.pow( 2, 10 * ( k -= 1 ) ) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) );
+    return a * Math.pow( 2, -10 * ( k -= 1 ) ) * Math.sin( ( k - s ) * ( 2 * Math.PI ) / p ) * 0.5 + 1;
+  };
+  easings.backIn = function(k) {
+    var s = 1.70158;
+    return k * k * ( ( s + 1 ) * k - s );
+  };
+  easings.backOut = function(k) {
+    var s = 1.70158;
+    return --k * k * ( ( s + 1 ) * k + s ) + 1;
+  };
+  easings.backInOut = function(k) {
+    var s = 1.70158 * 1.525;
+    if ( ( k *= 2 ) < 1 ) return 0.5 * ( k * k * ( ( s + 1 ) * k - s ) );
+    return 0.5 * ( ( k -= 2 ) * k * ( ( s + 1 ) * k + s ) + 2 );
+  };
+  easings.bounceIn = function(k) {
+    return 1 - easings.bounceOut( 1 - k );
+  };
+  easings.bounceOut = function(k) {
+    if ( k < ( 1 / 2.75 ) ) {
+      return 7.5625 * k * k;
+    } else if ( k < ( 2 / 2.75 ) ) {
+      return 7.5625 * ( k -= ( 1.5 / 2.75 ) ) * k + 0.75;
+    } else if ( k < ( 2.5 / 2.75 ) ) {
+      return 7.5625 * ( k -= ( 2.25 / 2.75 ) ) * k + 0.9375;
+    } else {
+      return 7.5625 * ( k -= ( 2.625 / 2.75 ) ) * k + 0.984375;
+    }
+  };
+  easings.bounceInOut = function(k) {
+    if ( k < 0.5 ) return easings.bounceIn( k * 2 ) * 0.5;
+    return easings.bounceOut( k * 2 - 1 ) * 0.5 + 0.5;
+  };
+})(window);
